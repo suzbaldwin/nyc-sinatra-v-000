ef53f697090aab95be2bfaf40f36d931a3ec50a6
suzy baldwin <gbaldwin24@comcast.net>
2018-06-11 22:39:56 +0000

    Done.

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,45 @@
+class FiguresController < ApplicationController
+
+  get '/figures/new' do
+
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(name: params["title_name"])
+    end
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(name: params["landmark_name"])
+    end
+    @figure.save
+    redirect "/figures/#{@figure.id}"
+  end
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end
+
+  post '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:id])
+
+    @figure.title = Title.find(name: params[:title][:name])
+    @figure.landmark = Landmark.find(name: params[:landmark][:name])
+    @figure.save
+    redirect "/figures/#{@figure.id}"
+
+  end
+end
